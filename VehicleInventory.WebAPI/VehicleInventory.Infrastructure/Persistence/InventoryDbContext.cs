120941edc763bd92f9a522a3cdbf82f9db30830b
Jaimin158 <Jaiminsolanki05@gmail.com>
2026-02-14 13:56:13 -0500

    Added vehicle repository and also made some changes in dbcontext file

--- VehicleInventory.WebAPI/VehicleInventory.Infrastructure/Persistence/InventoryDbContext.cs
+++ VehicleInventory.WebAPI/VehicleInventory.Infrastructure/Persistence/InventoryDbContext.cs
@@ -5,38 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using VehicleInventory.Domain.Entities;
-
 namespace VehicleInventory.Infrastructure.Persistence
 {
-    internal class InventoryDbContext : DbContext
+    public class InventoryDbContext : DbContext
     {
-        public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options) { }
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+            : base(options)
+        {
+        }
 
+        // Vehicles table
         public DbSet<Vehicle> Vehicles => Set<Vehicle>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Vehicle>(entity =>
             {
+                
                 entity.ToTable("Vehicles");
+                
                 entity.HasKey(v => v.Id);
-
+                
                 entity.Property(v => v.VehicleCode)
-                    .IsRequired()
-                    .HasMaxLength(30);
+                      .IsRequired()
+                      .HasMaxLength(30);
 
                 entity.HasIndex(v => v.VehicleCode)
-                    .IsUnique();
-
+                      .IsUnique();
                 entity.Property(v => v.VehicleType)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
+                      .IsRequired()
+                      .HasMaxLength(50);
                 entity.Property(v => v.LocationId)
-                    .IsRequired();
+                      .IsRequired();
 
                 entity.Property(v => v.Status)
-                    .IsRequired();
+                      .IsRequired();
             });
         }
     }
