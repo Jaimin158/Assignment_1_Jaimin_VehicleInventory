63edf8559baa3259850a3c2ec9bf9edff1878320
Jaimin158 <Jaiminsolanki05@gmail.com>
2026-02-14 02:16:10 -0500

    added dbcontext file

--- VehicleInventory.WebAPI/VehicleInventory.Infrastructure/Persistence/InventoryDbContext.cs
+++ VehicleInventory.WebAPI/VehicleInventory.Infrastructure/Persistence/InventoryDbContext.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VehicleInventory.Domain.Entities;
 
 namespace VehicleInventory.Infrastructure.Persistence
 {
-    internal class InventoryDbContext
+    internal class InventoryDbContext : DbContext
     {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options) { }
+
+        public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vehicle>(entity =>
+            {
+                entity.ToTable("Vehicles");
+                entity.HasKey(v => v.Id);
+
+                entity.Property(v => v.VehicleCode)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.HasIndex(v => v.VehicleCode)
+                    .IsUnique();
+
+                entity.Property(v => v.VehicleType)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(v => v.LocationId)
+                    .IsRequired();
+
+                entity.Property(v => v.Status)
+                    .IsRequired();
+            });
+        }
     }
 }
